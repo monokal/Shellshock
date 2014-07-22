872b143c02fb69eb73fd508b661ba226ac8ce15e
Daniel Middleton <me@daniel-middleton.com>
2014-07-22 22:45:45 +0100

    Work in progress.

--- shellshock.rb
+++ shellshock.rb
@@ -1,18 +1,20 @@
 #!/usr/bin/env ruby
 
+# Shellshock by Daniel-Middleton.com
+# Inspired by danielrehn and seanlinsley of GitHub.
 # Usage: Add "ruby ~/shellshock.rb" to ".bash_logout" in your home directory.
 
 begin
 
-  message = 'SEE YOU SPACE COWBOY'
-
+  # Method to display colours
   class String
     def colorize(color_code)
       "\e[#{color_code}m#{self}\e[0m"
     end
   end
 
-  rainbow = Array.new(21)
+  # Array of rainbow colours
+  rainbow = Array.new
   rainbow.push('38;5;160;01')
   rainbow.push('38;5;196;01')
   rainbow.push('38;5;202;01')
@@ -35,27 +37,88 @@ begin
   rainbow.push('38;5;057;01')
   rainbow.push('38;5;093;01')
 
-  puts '  .d8888b.  8888888888 8888888888      Y88b   d88P  .d88888b.  888     888 '.colorize
-  puts ' d88P  Y88b 888        888              Y88b d88P  d88P" "Y88b 888     888 '.colorize
-  puts '  "Y888b.   8888888    8888888            Y888P    888     888 888     888 '.colorize
-  puts '     "Y88b. 888        888                 888     888     888 888     888 '.colorize
-  puts '       "888 888        888                 888     888     888 888     888 '.colorize
-  puts ' Y88b  d88P 888        888                 888     Y88b. .d88P Y88b. .d88P '.colorize
-  puts '  "Y8888P"  8888888888 8888888888          888      "Y88888P"   "Y88888P"  '.colorize
-  puts '  .d8888b.  8888888b.     d8888  .d8888b.  8888888888                      '.colorize
-  puts ' d88P  Y88b 888   Y88b   d88888 d88P  Y88b 888                             '.colorize
-  puts '  "Y888b.   888   d88P d88P 888 888        8888888                         '.colorize
-  puts '     "Y88b. 8888888P" d88P  888 888        888                             '.colorize
-  puts '       "888 888      d88P   888 888    888 888                             '.colorize
-  puts ' Y88b  d88P 888     d8888888888 Y88b  d88P 888                             '.colorize
-  puts '  "Y8888P"  888    d88P     888  "Y8888P"  8888888888                      '.colorize
-  puts '  .d8888b.   .d88888b.  888       888 888888b.    .d88888b. Y88b   d88P    '.colorize
-  puts ' d88P  Y88b d88P" "Y88b 888   o   888 888  "88b  d88P" "Y88b Y88b d88P     '.colorize
-  puts ' 888        888     888 888 d888b 888 8888888K.  888     888   Y888P       '.colorize
-  puts ' 888        888     888 888d88888b888 888  "Y88b 888     888    888        '.colorize
-  puts ' 888    888 888     888 88888P Y88888 888    888 888     888    888        '.colorize
-  puts ' Y88b  d88P Y88b. .d88P 8888P   Y8888 888   d88P Y88b. .d88P    888        '.colorize
-  puts '  "Y8888P"   "Y88888P"  888P     Y888 8888888P"   "Y88888P"     888        '.colorize
+  # Array of rainbow colours in reverse
+  reverse_rainbow = rainbow.reverse
+
+  # Array of message lines
+  message = Array.new
+  message.push('  .d8888b.  8888888888 8888888888      Y88b   d88P  .d88888b.  888     888 ')
+  message.push(' d88P  Y88b 888        888              Y88b d88P  d88P" "Y88b 888     888 ')
+  message.push('  "Y888b.   8888888    8888888            Y888P    888     888 888     888 ')
+  message.push('     "Y88b. 888        888                 888     888     888 888     888 ')
+  message.push('       "888 888        888                 888     888     888 888     888 ')
+  message.push(' Y88b  d88P 888        888                 888     Y88b. .d88P Y88b. .d88P ')
+  message.push('  "Y8888P"  8888888888 8888888888          888      "Y88888P"   "Y88888P"  ')
+  message.push('  .d8888b.  8888888b.     d8888  .d8888b.  8888888888                      ')
+  message.push(' d88P  Y88b 888   Y88b   d88888 d88P  Y88b 888                             ')
+  message.push('  "Y888b.   888   d88P d88P 888 888        8888888                         ')
+  message.push('     "Y88b. 8888888P" d88P  888 888        888                             ')
+  message.push('       "888 888      d88P   888 888    888 888                             ')
+  message.push(' Y88b  d88P 888     d8888888888 Y88b  d88P 888                             ')
+  message.push('  "Y8888P"  888    d88P     888  "Y8888P"  8888888888                      ')
+  message.push('  .d8888b.   .d88888b.  888       888 888888b.    .d88888b. Y88b   d88P    ')
+  message.push(' d88P  Y88b d88P" "Y88b 888   o   888 888  "88b  d88P" "Y88b Y88b d88P     ')
+  message.push(' 888        888     888 888 d888b 888 8888888K.  888     888   Y888P       ')
+  message.push(' 888        888     888 888d88888b888 888  "Y88b 888     888    888        ')
+  message.push(' 888    888 888     888 88888P Y88888 888    888 888     888    888        ')
+  message.push(' Y88b  d88P Y88b. .d88P 8888P   Y8888 888   d88P Y88b. .d88P    888        ')
+  message.push('  "Y8888P"   "Y88888P"  888P     Y888 8888888P"   "Y88888P"     888        ')
+  message.push('  .d8888b.  8888888888 8888888888      Y88b   d88P  .d88888b.  888     888 ')
+  message.push(' d88P  Y88b 888        888              Y88b d88P  d88P" "Y88b 888     888 ')
+  message.push('  "Y888b.   8888888    8888888            Y888P    888     888 888     888 ')
+  message.push('     "Y88b. 888        888                 888     888     888 888     888 ')
+  message.push('       "888 888        888                 888     888     888 888     888 ')
+  message.push(' Y88b  d88P 888        888                 888     Y88b. .d88P Y88b. .d88P ')
+  message.push('  "Y8888P"  8888888888 8888888888          888      "Y88888P"   "Y88888P"  ')
+  message.push('  .d8888b.  8888888b.     d8888  .d8888b.  8888888888                      ')
+  message.push(' d88P  Y88b 888   Y88b   d88888 d88P  Y88b 888                             ')
+  message.push('  "Y888b.   888   d88P d88P 888 888        8888888                         ')
+  message.push('     "Y88b. 8888888P" d88P  888 888        888                             ')
+  message.push('       "888 888      d88P   888 888    888 888                             ')
+  message.push(' Y88b  d88P 888     d8888888888 Y88b  d88P 888                             ')
+  message.push('  "Y8888P"  888    d88P     888  "Y8888P"  8888888888                      ')
+  message.push('  .d8888b.   .d88888b.  888       888 888888b.    .d88888b. Y88b   d88P    ')
+  message.push(' d88P  Y88b d88P" "Y88b 888   o   888 888  "88b  d88P" "Y88b Y88b d88P     ')
+  message.push(' 888        888     888 888 d888b 888 8888888K.  888     888   Y888P       ')
+  message.push(' 888        888     888 888d88888b888 888  "Y88b 888     888    888        ')
+  message.push(' 888    888 888     888 88888P Y88888 888    888 888     888    888        ')
+  message.push(' Y88b  d88P Y88b. .d88P 8888P   Y8888 888   d88P Y88b. .d88P    888        ')
+  message.push('  "Y8888P"   "Y88888P"  888P     Y888 8888888P"   "Y88888P"     888        ')
+  message.push('  .d8888b.  8888888888 8888888888      Y88b   d88P  .d88888b.  888     888 ')
+  message.push(' d88P  Y88b 888        888              Y88b d88P  d88P" "Y88b 888     888 ')
+  message.push('  "Y888b.   8888888    8888888            Y888P    888     888 888     888 ')
+  message.push('     "Y88b. 888        888                 888     888     888 888     888 ')
+  message.push('       "888 888        888                 888     888     888 888     888 ')
+  message.push(' Y88b  d88P 888        888                 888     Y88b. .d88P Y88b. .d88P ')
+  message.push('  "Y8888P"  8888888888 8888888888          888      "Y88888P"   "Y88888P"  ')
+  message.push('  .d8888b.  8888888b.     d8888  .d8888b.  8888888888                      ')
+  message.push(' d88P  Y88b 888   Y88b   d88888 d88P  Y88b 888                             ')
+  message.push('  "Y888b.   888   d88P d88P 888 888        8888888                         ')
+  message.push('     "Y88b. 8888888P" d88P  888 888        888                             ')
+  message.push('       "888 888      d88P   888 888    888 888                             ')
+  message.push(' Y88b  d88P 888     d8888888888 Y88b  d88P 888                             ')
+  message.push('  "Y8888P"  888    d88P     888  "Y8888P"  8888888888                      ')
+  message.push('  .d8888b.   .d88888b.  888       888 888888b.    .d88888b. Y88b   d88P    ')
+  message.push(' d88P  Y88b d88P" "Y88b 888   o   888 888  "88b  d88P" "Y88b Y88b d88P     ')
+  message.push(' 888        888     888 888 d888b 888 8888888K.  888     888   Y888P       ')
+  message.push(' 888        888     888 888d88888b888 888  "Y88b 888     888    888        ')
+  message.push(' 888    888 888     888 88888P Y88888 888    888 888     888    888        ')
+  message.push(' Y88b  d88P Y88b. .d88P 8888P   Y8888 888   d88P Y88b. .d88P    888        ')
+  message.push('  "Y8888P"   "Y88888P"  888P     Y888 8888888P"   "Y88888P"     888        ')
+
+  col_i = 0
+
+  message.each { |msg|
+
+    puts msg.colorize(rainbow.at(col_i))
+
+    if col_i < rainbow.length
+      col_i += 1
+    elsif col_i == rainbow.length
+      rainbow = reverse_rainbow
+      col_i = 0
+    end
+  }
 
 rescue
   puts 'Error in shellshock.rb'
