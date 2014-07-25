7d488da4cf20429a2e2732e5e6e9d98fafe729a1
Daniel Middleton <dmiddleton@plus.net>
2014-07-25 20:19:11 +0100

    Added multi-message and random message selection support.

--- shellshock.rb
+++ shellshock.rb
@@ -13,51 +13,12 @@ begin
   # Declare vars
   message = Array.new
   colour  = Array.new
+  messages = Dir["messages/*"]
 
-  # Array of message lines - add as many lines as you like!
-  message.push('  .d8888b.  8888888888 8888888888      Y88b   d88P  .d88888b.  888     888 ')
-  message.push(' d88P  Y88b 888        888              Y88b d88P  d88P" "Y88b 888     888 ')
-  message.push('  "Y888b.   8888888    8888888            Y888P    888     888 888     888 ')
-  message.push('     "Y88b. 888        888                 888     888     888 888     888 ')
-  message.push('       "888 888        888                 888     888     888 888     888 ')
-  message.push(' Y88b  d88P 888        888                 888     Y88b. .d88P Y88b. .d88P ')
-  message.push('  "Y8888P"  8888888888 8888888888          888      "Y88888P"   "Y88888P"  ')
-  message.push('  .d8888b.  8888888b.     d8888  .d8888b.  8888888888                      ')
-  message.push(' d88P  Y88b 888   Y88b   d88888 d88P  Y88b 888                             ')
-  message.push('  "Y888b.   888   d88P d88P 888 888        8888888                         ')
-  message.push('     "Y88b. 8888888P" d88P  888 888        888                             ')
-  message.push('       "888 888      d88P   888 888    888 888                             ')
-  message.push(' Y88b  d88P 888     d8888888888 Y88b  d88P 888                             ')
-  message.push('  "Y8888P"  888    d88P     888  "Y8888P"  8888888888                      ')
-  message.push('  .d8888b.   .d88888b.  888       888 888888b.    .d88888b. Y88b   d88P    ')
-  message.push(' d88P  Y88b d88P" "Y88b 888   o   888 888  "88b  d88P" "Y88b Y88b d88P     ')
-  message.push(' 888        888     888 888 d888b 888 8888888K.  888     888   Y888P       ')
-  message.push(' 888        888     888 888d88888b888 888  "Y88b 888     888    888        ')
-  message.push(' 888    888 888     888 88888P Y88888 888    888 888     888    888        ')
-  message.push(' Y88b  d88P Y88b. .d88P 8888P   Y8888 888   d88P Y88b. .d88P    888        ')
-  message.push('  "Y8888P"   "Y88888P"  888P     Y888 8888888P"   "Y88888P"     888        ')
-  message.push('')
-  message.push('  .d8888b.  8888888888 8888888888      Y88b   d88P  .d88888b.  888     888 ')
-  message.push(' d88P  Y88b 888        888              Y88b d88P  d88P" "Y88b 888     888 ')
-  message.push('  "Y888b.   8888888    8888888            Y888P    888     888 888     888 ')
-  message.push('     "Y88b. 888        888                 888     888     888 888     888 ')
-  message.push('       "888 888        888                 888     888     888 888     888 ')
-  message.push(' Y88b  d88P 888        888                 888     Y88b. .d88P Y88b. .d88P ')
-  message.push('  "Y8888P"  8888888888 8888888888          888      "Y88888P"   "Y88888P"  ')
-  message.push('  .d8888b.  8888888b.     d8888  .d8888b.  8888888888                      ')
-  message.push(' d88P  Y88b 888   Y88b   d88888 d88P  Y88b 888                             ')
-  message.push('  "Y888b.   888   d88P d88P 888 888        8888888                         ')
-  message.push('     "Y88b. 8888888P" d88P  888 888        888                             ')
-  message.push('       "888 888      d88P   888 888    888 888                             ')
-  message.push(' Y88b  d88P 888     d8888888888 Y88b  d88P 888                             ')
-  message.push('  "Y8888P"  888    d88P     888  "Y8888P"  8888888888                      ')
-  message.push('  .d8888b.   .d88888b.  888       888 888888b.    .d88888b. Y88b   d88P    ')
-  message.push(' d88P  Y88b d88P" "Y88b 888   o   888 888  "88b  d88P" "Y88b Y88b d88P     ')
-  message.push(' 888        888     888 888 d888b 888 8888888K.  888     888   Y888P       ')
-  message.push(' 888        888     888 888d88888b888 888  "Y88b 888     888    888        ')
-  message.push(' 888    888 888     888 88888P Y88888 888    888 888     888    888        ')
-  message.push(' Y88b  d88P Y88b. .d88P 8888P   Y8888 888   d88P Y88b. .d88P    888        ')
-  message.push('  "Y8888P"   "Y88888P"  888P     Y888 8888888P"   "Y88888P"     888        ')
+  # Array of message lines from a random message
+  File.readlines("#{messages.sample}").each do |line|
+    message.push(line.chomp)
+  end
 
   # Array of rainbow colours
   colour.push('38;5;160;01','38;5;196;01','38;5;202;01','38;5;208;01','38;5;214;01',
@@ -76,7 +37,7 @@ begin
 
   # For each line of the message, use the next shade of colour.
   # When we hit the last shade, reverse the order of colours
-  # as to achieve a constant rainbow blending effect.
+  # as to achieve a constant blending rainbow effect.
   i = 0
   message.each { |msg|
     puts msg.colorize(colour.at(i))
